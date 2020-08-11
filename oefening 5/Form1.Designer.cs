2267d342cdf0cb6d5447e520eba70343b9166e95
yannick keil <keilyannick@hotmail.com>
2020-08-11 15:46:11 +0200

    oefening 5 klaar

---
+++ oefening 5/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace oefening_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbText = new System.Windows.Forms.ListBox();
+            this.tbBijvoegen = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbText
+            // 
+            this.lbText.FormattingEnabled = true;
+            this.lbText.ItemHeight = 20;
+            this.lbText.Location = new System.Drawing.Point(27, 25);
+            this.lbText.Name = "lbText";
+            this.lbText.Size = new System.Drawing.Size(163, 264);
+            this.lbText.TabIndex = 0;
+            // 
+            // tbBijvoegen
+            // 
+            this.tbBijvoegen.Location = new System.Drawing.Point(196, 25);
+            this.tbBijvoegen.Name = "tbBijvoegen";
+            this.tbBijvoegen.Size = new System.Drawing.Size(270, 26);
+            this.tbBijvoegen.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(27, 350);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(163, 38);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load from file";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(27, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(163, 36);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save to file";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(196, 135);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(115, 41);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(196, 67);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 41);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.tbBijvoegen);
+            this.Controls.Add(this.lbText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbText;
+        private System.Windows.Forms.TextBox tbBijvoegen;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ oefening 5/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oefening_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lbText.Items.Add(tbBijvoegen.Text);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            lbText.Items.RemoveAt(lbText.SelectedIndex);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("oefening5.txt"))
+            {
+                foreach (var item in lbText.Items)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lbText.Items.Clear();
+            using (StreamReader reader = new StreamReader("oefening5.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lbText.Items.Add(reader.ReadLine());
+                }
+            }
+        }
+    }
+}
