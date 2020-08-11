853314dd0fa750d3b975cf25fe63e36c99698d0a
yannick keil <keilyannick@hotmail.com>
2020-08-11 12:37:17 +0200

    oefening 3

---
+++ oefening 3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace oefening_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDatum = new System.Windows.Forms.DateTimePicker();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpDatum
+            // 
+            this.dtpDatum.Location = new System.Drawing.Point(12, 52);
+            this.dtpDatum.Name = "dtpDatum";
+            this.dtpDatum.Size = new System.Drawing.Size(251, 26);
+            this.dtpDatum.TabIndex = 0;
+            this.dtpDatum.Value = new System.DateTime(2020, 8, 11, 11, 56, 10, 0);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(363, 56);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(105, 32);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.dtpDatum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDatum;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
